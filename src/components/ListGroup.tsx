1d9cdb6c8456d1e544b9a9f160216911ee296966
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-05 21:10:43 +0545

    de-structure prop

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,13 +4,13 @@ interface Props {
   items: string[];
   heading: string;
 }
-const ListGroup = (props: Props) => {
+const ListGroup = ({ items, heading }: Props) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-      <h1>{props.heading}</h1>
+      <h1>{heading}</h1>
       <ul className="list-group">
-        {props.items.map((todo, index) => (
+        {items.map((todo, index) => (
           <li
             className={
               selectedIndex === index
