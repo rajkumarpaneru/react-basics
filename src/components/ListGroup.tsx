33409e1f83ee892bd12b587d27e0f145a3e61148
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-05 20:56:12 +0545

    activate list on click using useState hook

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ListGroup = () => {
   const todos = ["Study", "Exercise", "Code", "Meditate"];
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>My Todo List</h1>
       <ul className="list-group">
         {todos.map((todo, index) => (
-          <li className="list-group-item" key={index}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {todo}
           </li>
         ))}
