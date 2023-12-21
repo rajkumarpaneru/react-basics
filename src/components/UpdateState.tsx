50194583e21522c18154f89206ea95efdd773c3b
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-21 22:26:27 +0545

    update array of objects

--- src/components/UpdateState.tsx
+++ src/components/UpdateState.tsx
@@ -16,6 +16,11 @@ const UpdateState = () => {
 
   const [tags, setTags] = useState(["happy", "cheerful"]);
 
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
   const handleClick = () => {
     const newDrink = {
       ...drink,
@@ -40,6 +45,9 @@ const UpdateState = () => {
 
     //update
     setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+
+    //mark first bug as fixed
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
   return (
     <div>
