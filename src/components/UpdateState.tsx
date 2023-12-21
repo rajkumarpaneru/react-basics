a353f471937587f83d6b3501cc6f0cb717d0ed61
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-21 22:13:33 +0545

    update nested object

--- src/components/UpdateState.tsx
+++ src/components/UpdateState.tsx
@@ -6,16 +6,32 @@ const UpdateState = () => {
     price: 5,
   });
 
+  const [customer, setCustomer] = useState({
+    name: "John",
+    address: {
+      city: "San Francisco",
+      zipCode: 12345,
+    },
+  });
+
   const handleClick = () => {
     const newDrink = {
       ...drink,
       price: 6,
     };
     setDrink(newDrink);
+
+    setCustomer({
+      ...customer,
+      address: { ...customer.address, zipCode: 54321 },
+    });
   };
   return (
     <div>
       {drink.price}
+      <br />
+      {customer.address.zipCode}
+      <br />
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
