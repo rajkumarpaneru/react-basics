6d0f17e446a172240877fe2a148a8c93d3b161f3
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-23 11:47:59 +0545

    basic form

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
 import ExpandableTextRefactored from "./components/ExpandableTextRefactored";
+import Form from "./components/Form";
 
 const App = () => {
   const handleClear = () => {
@@ -19,15 +20,7 @@ const App = () => {
 
   return (
     <div>
-      <ExpandableTextRefactored count={20}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </ExpandableTextRefactored>
+      <Form />
     </div>
   );
 };
