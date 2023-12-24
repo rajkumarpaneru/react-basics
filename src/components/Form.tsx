062d8fed5ca779a32131ffcb8733c45031d14ea5
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-24 13:55:13 +0545

    form validation with zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,17 +1,24 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 characters." }),
+  age: z
+    .number({ invalid_type_error: "Age field is required." })
+    .min(1, { message: "Age must be greater than or equal to 1." })
+    .max(150, { message: "Age must be less than or equal to 150." }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>();
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -24,20 +31,12 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", {
-            required: true,
-            minLength: 3,
-          })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required.</p>
-        )}
-        {errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 3 characters.</p>
-        )}
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
       </div>
 
       <div className="mb-3">
@@ -45,24 +44,13 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age", {
-            required: true,
-            min: 1,
-            max: 100,
-          })}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
-        {errors.age?.type === "required" && (
-          <p className="text-danger">The age field is required.</p>
-        )}
-        {errors.age?.type === "min" && (
-          <p className="text-danger">The age should not be lower than 1.</p>
-        )}
-        {errors.age?.type === "max" && (
-          <p className="text-danger">The age should not be greater than 150.</p>
-        )}
+
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
