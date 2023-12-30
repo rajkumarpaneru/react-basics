48a8fb4685cd62f16c519488a6542f1553cb079d
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-12-30 20:57:04 +0545

    remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
-import Like from "./components/Like";
-import UpdateState from "./components/UpdateState";
-import Navbar from "./components/Navbar";
-import Cart from "./components/Cart";
-import ExpandableText from "./components/ExpandableText";
-import ExpandableTextRefactored from "./components/ExpandableTextRefactored";
-import Form from "./components/Form";
-import ProductList from "./components/ProductList";
 import axios, { AxiosError, CanceledError } from "axios";
-
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
 
 interface User {
   id: number;
